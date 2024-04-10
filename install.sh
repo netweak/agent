2336edcf2e9e55b289ab41dcff92ae010f5cf86b
Lucas Decrock <lucas@decrock.me>
2024-04-10 15:33:55 +0200

    Autorun after install

--- install.sh
+++ install.sh
@@ -140,6 +140,9 @@ then
 	crontab -u netweak -l 2>/dev/null | { cat; echo "* * * * * bash /etc/netweak/heartbeat.sh > /etc/netweak/log/cron.log 2>&1"; } | crontab -u netweak -
 	crontab -u netweak -l 2>/dev/null | { cat; echo "* * * * * bash /etc/netweak/agent.sh > /etc/netweak/log/cron.log 2>&1"; } | crontab -u netweak -
 	
+	# Initial run of agent
+	bash /etc/netweak/agent.sh
+
 	# Show success
 	echo -e "|\n|   Success: The Netweak agent has been installed\n|"
 	
